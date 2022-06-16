e1de810d0069085601bb014a1affcbcc5c286935
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-06-16 16:02:11 -0400

    Fix:Element that is called 2 times in JS is removed

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -9,33 +9,10 @@ const calculate = function () {
   value_tot.innerHTML = price.innerHTML * amount.value
   amount_tot.innerHTML = amount.value
 }
-
 const the_color = function () {
-  let element = document.querySelector('#selector')
   let circle_color = document.querySelector('#color_select')
-  circle_color.style.backgroundColor = element.value
+  circle_color.style.backgroundColor = selector.value
 }
 
 button.addEventListener('click', calculate)
 selector.addEventListener('change', the_color)
-
-// Add listener
-
-//  button.removeEventListener("click", funcName);    // Delete listener
-
-/*
-function calculate() {
-  var price = document.querySelector('.value_value')
-  var amount = document.querySelector('.amount_in')
-  var value_tot = document.querySelector('#value_total')
-  var amount_tot = document.querySelector('#amount_total')
-  value_tot.innerHTML = price.innerHTML * amount.value
-  amount_tot.innerHTML = amount.value
-}
-
-function the_color() {
-  var element = document.querySelector('#selector')
-  var circle_color = document.querySelector('#color_select')
-  circle_color.style.backgroundColor = element.value
-}
-*/
