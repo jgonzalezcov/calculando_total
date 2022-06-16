eeb1d7a31e2f40a7a1e41b7e3174098d32bee8a2
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-06-16 15:57:28 -0400

    Fix:Changed the way functions are called from HTML to JS

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,4 +1,29 @@
 'use strict'
+const button = document.querySelector('button')
+const selector = document.querySelector('select')
+const calculate = function () {
+  let price = document.querySelector('.value_value')
+  let amount = document.querySelector('.amount_in')
+  let value_tot = document.querySelector('#value_total')
+  let amount_tot = document.querySelector('#amount_total')
+  value_tot.innerHTML = price.innerHTML * amount.value
+  amount_tot.innerHTML = amount.value
+}
+
+const the_color = function () {
+  let element = document.querySelector('#selector')
+  let circle_color = document.querySelector('#color_select')
+  circle_color.style.backgroundColor = element.value
+}
+
+button.addEventListener('click', calculate)
+selector.addEventListener('change', the_color)
+
+// Add listener
+
+//  button.removeEventListener("click", funcName);    // Delete listener
+
+/*
 function calculate() {
   var price = document.querySelector('.value_value')
   var amount = document.querySelector('.amount_in')
@@ -13,3 +38,4 @@ function the_color() {
   var circle_color = document.querySelector('#color_select')
   circle_color.style.backgroundColor = element.value
 }
+*/
