5eb52d2865e59e42a468f1733524ba9c36c50626
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-06-30 21:00:35 -0400

    Fix:The code is optimized leaving it only in 4 lines

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,16 +1,4 @@
-'use strict'
-const button = document.querySelector('button')
-const selector = document.querySelector('select')
-const calculate = function () {
-  let price = document.querySelector('.value_value')
-  let amount = document.querySelector('.amount_in')
-  document.querySelector('#value_total').innerHTML =
-    price.innerHTML * amount.value
-  document.querySelector('#amount_total').innerHTML = amount.value
-}
-const the_color = function () {
-  document.querySelector('#color_select').style.backgroundColor = selector.value
-}
-
-button.addEventListener('click', calculate)
-selector.addEventListener('change', the_color)
+document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('#value_total').innerHTML = document.querySelector('.value_value').value * document.querySelector('.amount_in').value
+  document.querySelector('#amount_total').innerHTML = document.querySelector('.amount_in').value
+  document.querySelector('#color_select').style.backgroundColor = selector.value})
